a781572ed3c5d5831b0c63077cee5cf510c5c81a
tysonparks <typarks@gmail.com>
2018-02-27 11:35:12 -0800

    Initial Commit

---
+++ Every.Do/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  Every.Do
+//
+//  Created by Tyson Parks on 2/27/18.
+//  Copyright © 2018 Tyson Parks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ Every.Do/MasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.h
+//  Every.Do
+//
+//  Created by Tyson Parks on 2/27/18.
+//  Copyright © 2018 Tyson Parks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
