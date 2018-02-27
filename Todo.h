b29f93afcfe190399f494770d827942069cd7734
tysonparks <typarks@gmail.com>
2018-02-27 11:36:42 -0800

    Created Todo class

---
+++ Todo.h
@@ -0,0 +1,13 @@
+//
+//  Todo.h
+//  Every.Do
+//
+//  Created by Tyson Parks on 2/27/18.
+//  Copyright © 2018 Tyson Parks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Todo : NSObject
+
+@end
---
+++ Todo.m
@@ -0,0 +1,13 @@
+//
+//  Todo.m
+//  Every.Do
+//
+//  Created by Tyson Parks on 2/27/18.
+//  Copyright © 2018 Tyson Parks. All rights reserved.
+//
+
+#import "Todo.h"
+
+@implementation Todo
+
+@end
